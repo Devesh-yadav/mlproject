d93620b45e43ad5d40e2ab934fd75903b22b38fb
unknown <devesh.yadav76@gmail.com>
2025-05-18 13:39:48 +0530

    size reduce of env req

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
-import numpy as np
+""" import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler """
 from src.utils import load_object
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
